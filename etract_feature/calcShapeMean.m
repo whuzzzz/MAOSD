e686f877683f451889d5dd5483ae0e5e8e424ff3
whuzzz <2023106190019@whu.edu.cn>
2024-12-01 14:34:14 +0800

    Add files via upload

---
+++ etract_feature/calcShapeMean.m
@@ -0,0 +1,7 @@
+function [desc] = calcShapeMean(seg, numRegion)
+    
+    stats = regionprops(seg, 'Area', 'Centroid');
+    desc.area = cat(1, stats.Area);
+    desc.center = cat(1, stats.Centroid);
+    
+end
